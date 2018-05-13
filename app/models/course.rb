ef1eb368d336a0e3223487ac3a78231ab50810ac
furetur <furetur@gmail.com>
2018-05-13 23:36:21 +0500

    added some simple calidations

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,9 +6,12 @@ class Course < ApplicationRecord
 
   # Попробуйте создать таблицу-справочник и свяжите на уровне базы данных и ActiveRecord пользователей и курс
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 1..20 }
 
   # На вас остается подумать, какой тут еще необходимый атрибут и как написать на него валидацию!
+
+  validates :description, presence: true, length: { in: 1..100 }
+  validates :curator_id, presense: true
 
 
   belongs_to :curator, class_name: 'User', foreign_key: 'curator_id'
