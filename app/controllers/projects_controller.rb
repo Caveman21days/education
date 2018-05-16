d4498b4ff136f1ed24b154c7cea833d2b390ee47
furetur <furetur@gmail.com>
2018-05-16 11:17:17 +0500

    Fixed the typo

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @projects = Project.find(params[:id])
+    @project = Project.find(params[:id])
   end
 
   #Можно и так, если ничего внутри нет
