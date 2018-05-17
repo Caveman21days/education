147a2deb1d59d33d0065846e3913344963b36356
furetur <furetur@gmail.com>
2018-05-17 23:59:40 +0500

    worked on users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+
+  resources :users
   resources :courses do
-    resources :projects, shallow: true
+
+    resources :projects, shallow: true do
+      resources :issue
+    end
 
     resources :trainings, shallow: true
   end
