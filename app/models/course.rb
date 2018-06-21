bbf45abb0a8e8c6daa77727e782664e8d369dd97
CavemaN21 <kotkidach@naumen.ru>
2018-06-21 18:41:27 +0500

    left panel

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,8 +2,10 @@ class Course < ApplicationRecord
   belongs_to :field
 
   has_many :user_assignments, as: :assignmentable, dependent: :destroy
+
   has_many :users
   has_many :issues
+
   has_many :users,  through: :user_assignments
-  has_many :issues, through: :user_assignments, dependent: :destroy
+  has_many :issues, through: :user_assignments
 end
