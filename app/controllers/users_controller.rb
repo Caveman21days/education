0caf3ba56610d04c88c51dd9eae5cd83f0058b09
furetur <furetur@gmail.com>
2018-06-29 00:41:01 +0500

    users n some shit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
   authorize_resource
+  before_action :set_user, only: [:show]
+
+
+  def index
+    @users = User.all
+  end
+
+
+  def show; end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
