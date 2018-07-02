b39c6df9d00eb4ed4fa950b6be2c64b9a81b9470
furetur <furetur@gmail.com>
2018-07-02 23:32:58 +0500

    some not working shit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,34 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
   def show; end
 
+  def new
+    respond_with @user = User.new
+  end
+
+  def create
+    respond_with(@user = User.create(user_params))
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
 
   private
+
+  def user_params
+    params.require(:user).permit(:f_name, :l_name, :email)
+  end
 
   def set_user
     @user = User.find(params[:id])
