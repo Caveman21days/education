cca586df3660e238ee04a1a5503387bf53f7ccae
furetur <furetur@gmail.com>
2018-07-06 17:44:15 +0500

    Dev (#12)
    * bootstrap import fixes, css for sign_in and sign_up
    
    * some views converted from erb to haml with bootstrap classes added
    
    * added .field where necessary
    
    * users n some shit
    
    * some not working shit
    
    * form fixes, controller, auto-complete
    
    * added some fields
    
    * small fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,40 @@
 class UsersController < ApplicationController
   authorize_resource
+  before_action :set_user, only: [:show, :edit, :update]
+
+
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def new
+    respond_with @user = User.new
+  end
+
+  def create
+    respond_with(@user = User.create(user_params))
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:f_name, :s_name, :l_name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
