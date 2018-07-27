cefc210089dd77ed63ba8727c2e9c62acd2f079c
NaNisNumber <maks.bezell@gmail.com>
2018-07-27 10:47:17 +0500

    redundant file removed

--- app/assets/javascripts/project.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const sendApplicationButton = document.querySelector('#send-application')
-
-  const sendApplication = () => {
-    
-  }
-
-});
