5634320342c97fa3d7d7d55f1c16b315ff17fc0d
furetur <furetur@gmail.com>
2018-08-08 16:13:23 +0500

    added nickname to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730093729) do
+ActiveRecord::Schema.define(version: 20180808065805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 20180730093729) do
     t.string "award"
     t.string "vk_profile"
     t.string "telegram_profile"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
